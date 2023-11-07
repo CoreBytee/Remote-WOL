d5c9c1feef42c7b2a0f9d9a386f8b6e0e1136e42
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-11-07 15:54:57 +0100

    send back

--- Client/Index.js
+++ Client/Index.js
@@ -2,3 +2,14 @@ const FS = require("fs")
 
 const WebSocket = new (require("./Classes/WebSocket"))()
 
+WebSocket.OnMessage(
+    function (Message) {
+        if (Message != "boot") {
+            return
+        }
+        console.log("SENDING WOL PACKET")
+        WebSocket.Send(
+            "looting"
+        )
+    }
+)
