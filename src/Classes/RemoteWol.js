2bdde2f1e994edbbfa37ff958618d98cd039727d
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2024-04-18 15:52:00 +0200

    allow usage of TARGET_IP_OVERRIDE

--- src/Classes/RemoteWol.js
+++ src/Classes/RemoteWol.js
@@ -4,9 +4,9 @@ export default class RemoteWol {
     constructor(TargetMac, TargetIP) {
         // State
         this.TargetMac = TargetMac
-        this.TargetIP = TargetIP
+        this.TargetIP = process.env.TARGET_IP_OVERRIDE || TargetIP
 
         // Objects
-        this.Server = new Server()
+        this.Server = new Server(this)
     }
 }
